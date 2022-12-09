a9607c9e92e192044549e015e5326d59fc0c53a7
WebDevelopo11 <si.sada93@gmail.com>
2022-12-09 15:32:52 +0100

    Assignment 4

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 const express = require('express');
 
 const path = require('path');
-
-const siteRoutes = require('./routes/site');
-const rootDir = require('./util/path');
+const bodyParser = require('body-parser');
 
 const app = express();
+const router = express.Router();
+
+const users = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(siteRoutes);
+router.get('/', (req, res, next) => {
+   res.render('home.ejs', {
+      pageTitle: 'Home',
+      url: '/'
+   });
+});
+
+router.get('/users', (req, res, next) => {
+   console.log(users.length);
+   res.render('users.ejs', {
+      pageTitle: 'Users',
+      url: '/users',
+      users: users
+   });
+});
+
+router.post('/add-user', (req, res, next) => {
+   users.push({
+      username: req.body.username
+   });
+   res.status(302).redirect('/users');
+});
+
+app.use(router)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+   res.status(404).render('404.ejs', { pageTitle: 'Page not found', url: '' });
 })
 
 app.listen(3000);
